f7ac0e8e7e05c33f6355c2a9e161c7b38e7d3f23
Kalyani Jadhav <kalyanijadhav114@gmail.com>
2025-07-28 23:07:01 +0530

    change-6

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: true,
     cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge: 7 * 24 * 60 * 60 * 1000,
+        expires: Date.now() + 24 * 60 * 60 * 1000,
+        maxAge: 24 * 60 * 60 * 1000,
         httpOnly: true  //prevent cross scripting attack
     }
 };
